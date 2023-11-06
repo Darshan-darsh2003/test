44f0c71f8eb0178fb13fcaa9f8d27bb0fd65e3fb
darshan <poojaridarsu9550@gmail.com>
2023-11-06 18:54:16 +0530

    App changes in jenkinsfile install

--- src/App.tsx
+++ src/App.tsx
@@ -14,45 +14,54 @@ function App() {
 
   return (
     <div className="App">
-      <div className="App-header">
-        <h2>SIMPLE REACT TEST</h2>
-      </div>
-      <p className="App-intro">
-        The state is <b>{count}</b>
-      </p>
-      <div
-        style={{
-          display: "flex",
-          gap: "20px",
-          width: "100%",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <button
+      <div className="container">
+        <div
+          className="App-header"
           style={{
-            width: "100px",
-            height: "60px",
-            outline: "none",
-            borderRadius: "10px",
-            color: "#000",
+            padding: "10px",
           }}
-          onClick={handleDecrement}
         >
-          Decrement -
-        </button>
-        <button
-          style={{
-            width: "100px",
-            height: "60px",
-            outline: "none",
-            borderRadius: "10px",
-            color: "#000",
-          }}
-          onClick={handleIncrement}
-        >
-          Increment +
-        </button>
+          <h2>SIMPLE REACT TEST</h2>
+        </div>
+        <div style={{ backgroundColor: "#fff", paddingBottom: "20px" }}>
+          <p className="App-intro">
+            The state is <b style={{ fontSize: "30px" }}>{count}</b>
+          </p>
+          <div
+            style={{
+              display: "flex",
+              gap: "20px",
+              width: "100%",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <button
+              style={{
+                width: "100px",
+                height: "60px",
+                outline: "none",
+                borderRadius: "10px",
+                color: "#000",
+              }}
+              onClick={handleDecrement}
+            >
+              Decrement -
+            </button>
+            <button
+              style={{
+                width: "100px",
+                height: "60px",
+                outline: "none",
+                borderRadius: "10px",
+                color: "#000",
+              }}
+              onClick={handleIncrement}
+            >
+              Increment +
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
